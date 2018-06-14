7ae60f05ee61b7456d26efc72ff7726e8a8beeb5
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-06-14 05:58:12 +0530

    loading images, caption, title, body from posts

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify, url_for, redirect
+import requests
+import json
+
+app = Flask(__name__)
+API_KEY = 'O0GrIgl5gKwiP9qz0xb09fuvjsASDzDSuHPqdH2fBD71WLNeTD'
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/search/<query>')
+def search(query):
+    url = 'https://api.tumblr.com/v2/tagged?tag=' + query + '&api_key=' + API_KEY + '&limit=10&filter=text'
+    response = requests.get(url)
+    return jsonify(response.json())
+
+@app.route('/blog')
+def returntoindex():
+    return redirect(url_for('index'))
+
+@app.route('/blog/<blogname>')
+def blog(blogname):
+    url = 'https://api.tumblr.com/v2/blog/' + blogname + '/info?api_key=' + API_KEY
+    response = requests.get(url)
+    return render_template('blog.html', response = response.json())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
